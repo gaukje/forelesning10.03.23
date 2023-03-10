7ca4fd54ad8629336a2e872a18f2d95d99377d38
gautte <s362066@oslomet.no>
2023-03-10 15:10:38 +0100

    argsparse arguments example

--- sysarg.py
+++ sysarg.py
@@ -9,6 +9,7 @@ if len(sys.argv) < 2:
     print ("i want at least two arguments")
     sys.exit()
 """
+
 parser = argparse.ArgumentParser(description="positional arguments")
 parser.add_argument('name', help="enter your name")
 parser.add_argument('num1', help="enter number 1" )
@@ -16,6 +17,17 @@ parser.add_argument('num2', help="enter number 2" )
 
 args = parser.parse_args()
 
-print ("your name is: ", args.name)
-print ("you entered number 1:", args.num1)
-print ("you entered number 2:", args.num2)
+print("your name is: ", args.name)
+
+print("you entered number 1:", args.num1)
+print("you entered number 2:", args.num2)
+
+
+parser.add_argument('-n1', '--num1', type=int)
+
+args = parser.parse_args()
+
+print("your number is: ", args.num1)
+print("your 2nd number is: ", args.num2)
+
+parser.add_argument('-s', '--server', action='store_true')
