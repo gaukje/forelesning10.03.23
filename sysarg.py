082d10e11de2dbe0e382ffa0b5e0774f3c4015d0
gautte <s362066@oslomet.no>
2023-03-10 15:02:28 +0100

    Initial commit

---
+++ sysarg.py
@@ -0,0 +1,21 @@
+import sys
+import argparse
+"""
+print ("this is name of your program", sys.argv[0])
+
+print ("total number of arguments", len(sys.argv))
+
+if len(sys.argv) < 2:
+    print ("i want at least two arguments")
+    sys.exit()
+"""
+parser = argparse.ArgumentParser(description="positional arguments")
+parser.add_argument('name', help="enter your name")
+parser.add_argument('num1', help="enter number 1" )
+parser.add_argument('num2', help="enter number 2" )
+
+args = parser.parse_args()
+
+print ("your name is: ", args.name)
+print ("you entered number 1:", args.num1)
+print ("you entered number 2:", args.num2)
